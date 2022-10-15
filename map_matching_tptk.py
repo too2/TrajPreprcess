ff6cfb82da4e2fe250bffe4e9eb9b311530896f1
too2 <3220555808@qq.com>
2022-10-15 10:49:36 +0800

    current version

---
+++ map_matching_tptk.py
@@ -0,0 +1,8 @@
+# 先保存为地图匹配能匹配的格式，将每个运单保存为一个文件
+# traj_data = pd.read_csv('/Volumes/T7/traj_file/taian/new_traj_flow.csv')
+# save_path = '/Volumes/T7/traj_file/taian/traj_cleaning/'
+# for waybill_no, value in tqdm(traj_data.groupby('waybill_no')):
+#     filename = str(waybill_no) + '.csv'
+#     value.to_csv(os.path.join(save_path, filename), index=False)
+
+# 此后步骤详见项目Projects
